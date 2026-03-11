6d3765921be29f0d0b93237b05345fc2946c3eae
parothegreat <alvaroprayogo38@gmail.com>
2026-03-11 11:45:12 +0700

    [deleted] comments

--- include/usb_keyboard.hpp
+++ include/usb_keyboard.hpp
@@ -1,13 +1,11 @@
 #ifndef USB_KEYBOARD_HPP
 #define USB_KEYBOARD_HPP
 
-// Wrapper header for EspTinyUSB keyboard functionality
-// This provides compatibility with the USB keyboard classes from the EspTinyUSB library
 
-// Include the HID keyboard header from EspTinyUSB
+
 #include <hidkeyboard.h>
 
-// Alias for convenience
+
 using USBKeyboard = HIDkeyboard;
 
-#endif // USB_KEYBOARD_HPP
+#endif 
